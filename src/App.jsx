eb7856ccf260dd8414bedcf588f949c9a7c1f943
deoclaw <115387064+deoclaw@users.noreply.github.com>
2024-02-22 00:21:08 -0600

    iterated through todo data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Todo from "./components/Todo";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(props) {
+	// straight up, I am reading Mozilla's MDN docs
+	// I am taking the aria tools as one thing I do want to better myself at is accessibility
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	//lets create an array of Todos mapped from our task prop passed through from main.jsx
+	//the ?. lets us perform optional chaining to check if props.tasks is undef. or null
+	//key is a special prop for react -- we must always use a UNIQUE KEY
+	const taskList = props.tasks?.map((task) => (
+		<Todo
+			id={task.id}
+			name={task.name}
+			completed={task.completed}
+			key={task.id}
+		/>
+	));
+	return (
+		<div className="todoapp stack-large">
+			<h1>ToDo List</h1>
+			<form>
+				<h2 className="label-wrapper">
+					<label htmlFor="new-todo-input" className="label__lg">
+						What're We Doing?
+					</label>
+				</h2>
+				<input
+					type="text"
+					id="new-todo-input"
+					className="input input__lg"
+					name="text"
+					autoComplete="off"
+				/>
+				<button type="submit" className="btn btn__primary btn__lg">
+					Add
+				</button>
+			</form>
+			<div className="filters btn-group stack-exception">
+				<button type="button" className="btn toggle-btn" aria-pressed="true">
+					<span className="visually-hidden">Show</span>
+					<span>All</span>
+					<span className="visually-hidden">tasks</span>
+				</button>
+				<button type="button" className="btn toggle-btn" aria-pressed="true">
+					<span className="visually-hidden">Show</span>
+					<span>Pending</span>
+					<span className="visually-hidden">tasks</span>
+				</button>
+				<button type="button" className="btn toggle-btn" aria-pressed="true">
+					<span className="visually-hidden">Show</span>
+					<span>Completed</span>
+					<span className="visually-hidden">tasks</span>
+				</button>
+			</div>
+			<h2 id="list-heading">Tasks Remaining</h2>
+			<ul
+				role="list"
+				className="todo-list stack-large stack-exception"
+				aria-labelledby="list-heading"
+			>
+				{taskList}
+			</ul>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// creating const to hold the array of data that is our ToDo list
+const TASKDATA = [
+	{ id: "todo-0", name: "Eat", completed: true },
+	{ id: "todo-1", name: "Sleep", completed: false },
+	{ id: "todo-2", name: "Shop", completed: false },
+];
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App tasks={TASKDATA} />
+	</React.StrictMode>
+);
